905e5601ddca7bf1aee13c254ae7e4bf9b26cdd2
Miguel <miguellukas52@gmail.com>
2021-02-16 14:35:28 -0300

    fixed the problem with youBot orientation

---
+++ lib/utils/angle.py
@@ -0,0 +1,50 @@
+#  * Copyright (c) 16/02/2021 13:32
+#  *
+#  * Last modified 16/02/2021 13:32
+#  * Miguel L. Rodrigues
+#  * All rights reserved
+
+from math import radians, sin, cos, atan2
+
+from lib.utils.matrix import Matrix
+
+phi = radians(-90)
+
+r_x = Matrix(3, 3)
+
+r_x.assign([
+    [1, 0, 0],
+    [0, cos(phi), -sin(phi)],
+    [0, sin(phi), cos(phi)]
+])
+
+
+def calculate_matrix_r(data):
+    matrix_r = Matrix(3, 3)
+
+    cos_theta = cos(data[3])
+    sin_theta = sin(data[3])
+
+    matrix_r.assign([
+        [(pow(data[0], 2) * (1 - cos_theta) + cos_theta),
+         ((data[0] * data[1]) * (1 - cos_theta) - (data[2] * sin_theta)),
+         ((data[0] * data[2]) * (1 - cos_theta) + (data[1] * sin_theta))],
+        [((data[0] * data[1]) * (1 - cos_theta) + (data[2] * sin_theta)),
+         (pow(data[1], 2) * (1 - cos_theta) + cos_theta),
+         ((data[1] * data[2]) * (1 - cos_theta) - (data[0] * sin_theta))],
+        [((data[0] * data[2]) * (1 - cos_theta) - (data[1] * sin_theta)),
+         ((data[1] * data[2]) * (1 - cos_theta) + (data[0] * sin_theta)),
+         (pow(data[2], 2) * (1 - cos_theta) + cos_theta)]
+    ])
+
+    return matrix_r
+
+
+def calculate_angle(process_variable):
+    matrix_r = calculate_matrix_r(process_variable)
+
+    result = r_x.transpose().multiply(matrix_r)
+
+    angle = atan2(result.get_value(1, 0), result.get_value(0, 0))
+
+    return angle
---
+++ test_main.py
@@ -0,0 +1,35 @@
+#  * Copyright (c) 16/02/2021 14:03
+#  *
+#  * Last modified 16/02/2021 14:03
+#  * Miguel L. Rodrigues
+#  * All rights reserved
+from lib.utils.angle import calculate_angle
+from lib.utils.pid import Pid
+from lib.utils.vector import normalize_radian, Vector
+from lib.webots_lib.wbc_controller import Controller
+from lib.youbot_control.youBot import YouBot
+
+
+cont = Controller(14, True)
+youBot = YouBot(cont)
+
+angle_pid = Pid(8.0, 0.07, 2.0, 5.0, 1.0)
+
+while cont.step() != -1:
+    youBot_position = youBot.get_position()
+    box_position = Vector(cont.get_object_position("box"))
+
+    youBot_rotation = cont.get_object_rotation("youBot")
+    box_rotation = cont.get_object_rotation("box")
+
+    angle = calculate_angle(youBot_rotation)
+
+    theta = youBot_position.differenceAngle(box_position)
+
+    angle_error = normalize_radian(angle + theta)
+
+    # angle_error = normalize_radian(box_rotation[3] - angle)
+    #
+    out = angle_pid.compute(angle_error, 0.05)
+
+    youBot.set_wheels_speed([-out, out, -out, out])
