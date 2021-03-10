cfc34d88f61bb66095c0f2d79a00832a3245505c
Miguel <miguellukas52@gmail.com>
2021-03-10 20:33:27 -0300

    add fitness plot

--- genetic.py
+++ genetic.py
@@ -4,45 +4,60 @@
 #  * Miguel L. Rodrigues
 #  * All rights reserved
 
-from lib.network.network import Network
+from lib.network.network import Network, load_network
 from lib.utils.vector import Vector
 from lib.webots_lib.wbc_controller import Controller
 from lib.youbot_control.youBot import YouBot
 from math import atan2, sin, cos
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot()
 
 
 def normalize(value: float) -> float:
     return atan2(sin(value), cos(value))
 
 
-cont = Controller(22, True)
+cont = Controller(14, True)
 youBot = YouBot(cont)
 
 networks = []
 
 errors = []
 
-max_per_generation = 5
-generations = 50
+max_per_generation = 10
+generations = 1000
 
 for i in range(max_per_generation):
     networks.append(Network([1, 8, 16, 16, 8, 1]))
 
 count = 0
 current = 0
+xx = [0]
 
 time_interval = 10
 last_time = 0
 
-max_velocity = 16
+max_velocity = 64
 
 network = networks[current]
+
+# network = load_network("network1.json")
 
 print("Geracao: {} de {}".format(count, generations))
 
 center = youBot.get_position()
+initial_position = center
 
 angle = .0
+
+comp = .01
+
+fitness_list = [.0]
+
+target_fit = .0001
 
 while cont.step() != -1:
     time = cont.get_supervisor().getTime()
@@ -51,7 +66,13 @@ while cont.step() != -1:
 
     youBot_rotation_angle = youBot.get_rotation_angle()
 
-    angle -= .01
+    angle += comp
+
+    if angle > 3.14:
+        comp = -.01
+
+    if angle < -3.14:
+        comp = .01
 
     x = 0.8 * cos(angle)
     z = 0.8 * sin(angle)
@@ -77,15 +98,26 @@ while cont.step() != -1:
             for error in errors:
                 sum_errors += abs(error)
 
-            fitness = sum_errors / len(errors)
+            fitness = (sum_errors / len(errors)) + initial_position.distance(youBot.get_position())
+
+            fit_err = 0.5 * pow(target_fit - fitness, 2.0)
+
+            if fit_err <= target_fit:
+                break
 
             network.set_fitness(fitness)
 
-            print("Individuo {} | Fitness {}".format(current, fitness))
+            fitness_list.append(fitness)
+
+            cont.set_object_position("youBot", [initial_position.x, initial_position.y, initial_position.z])
+
+            print("Individuo {} | Fitness {} | Fit Err {}".format(current, fitness, fit_err))
 
             errors.clear()
 
             current += 1
+
+            xx.append(xx[-1] + 1.0)
 
             if current == len(networks):
                 count += 1
@@ -123,4 +155,10 @@ while cont.step() != -1:
     youBot.set_wheels_speed([speed, -speed, speed, -speed])
 
 
-# network.save("network.json")
+network.save("network1.json")
+
+ax1.clear()
+
+ax1.plot(xx, fitness_list)
+
+plt.show()
